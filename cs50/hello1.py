1c20d1a77ae89ad7c0f7bc0449bcdd099e820778
02CodingHero <eo8@tuta.io>
2025-01-03 22:23:30 -0500

    update

---
+++ cs50/hello1.py
@@ -0,0 +1,18 @@
+def main():
+    name = input("What's your name? ")
+    print(hello(name))
+
+def hello(to="world"):
+    return f"hello, {to}"
+
+
+if __name__ =="__main__":
+    main()
+
+
+# def main():
+#     print("Hello, world!")
+#     print("This is CS50P.")
+
+
+# main()
