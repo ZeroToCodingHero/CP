cd970dacdecdf7a388943e267439ce4a40765dd9
02CodingHero <eo8@tuta.io>
2025-02-24 12:49:54 -0500

    update

--- practice/test.py
+++ practice/test.py
@@ -1,14 +1,17 @@
-import random
-
-def dice_roll():
-    available_choices = range(7)
-    player = random.choice(available_choices)
-    print(player)
-
-dice_roll()
-
+print("Testing\nGround")
 
 x = 5
-y = 2
-result = x ** y
-print(result)
+y = 5
+z = x + y
+print(z)
+
+x, y = 5, 5
+z = x + y
+print(z)
+
+def num_num():
+    x, y = 5, 5
+    z = x + y
+    print(z)
+
+num_num()
