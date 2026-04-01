1202e789a0816d402fe1d2cab393efb11287ecc4
02CodingHero <eo8@tuta.io>
2026-04-01 15:10:59 -0400

    update

--- python/cs50/week-0/def.py
+++ python/cs50/week-0/def.py
@@ -1,24 +1,19 @@
-def main():
+def name():
     name = input("What's your name? ").strip().title()
     hello(name)
-
 
 def hello(to="world"):
     print("Hello,", to)
 
-
-main()
-
+name()
 
 # hello()
 # name = input("What's your name? ")
 # hello(name)
 
-
 def main():
     print("Hello world")
     print("This is CS50.")
-
 
 main()
 
